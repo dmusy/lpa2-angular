d06f5d2cad533c76d40b3ae2ecd848a0460cdced
DamienM39 <damienmusy@sfr.fr>
2020-04-26 19:28:42 +0200

    issue1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Burgers'`, () => {
+  it(`should have as title 'burger'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Burgers');
+    expect(app.title).toEqual('burger');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Burgers app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('burger app is running!');
   });
 });
