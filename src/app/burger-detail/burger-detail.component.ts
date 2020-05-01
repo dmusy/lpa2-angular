abd0aa6eb4bb4beb845aca972f49f51b25fd6af3
DamienM39 <damienmusy@sfr.fr>
2020-05-01 16:52:47 +0200

    update

---
+++ src/app/burger-detail/burger-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { BurgersService } from '../api_burger/api/burgers.service';
+
+@Component({
+  selector: 'app-burger-detail',
+  templateUrl: './burger-detail.component.html',
+  styleUrls: ['./burger-detail.component.css']
+})
+export class BurgerDetailComponent implements OnInit {
+
+  burger;
+
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private burgersService : BurgersService
+    ) { }
+
+  ngOnInit(): void {
+    this.getBurger();
+  }
+
+  getBurger() : void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.burger = this.burgersService.burgerDetail(id)
+        .subscribe(burger => this.burger = burger);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }  
+}
---
+++ src/app/burger-list/burger-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { BurgersService } from '../api_burger/api/burgers.service';
+
+@Component({
+  selector: 'app-burger-list',
+  templateUrl: './burger-list.component.html',
+  styleUrls: ['./burger-list.component.css']
+})
+export class BurgerListComponent implements OnInit {
+
+  burgerList;
+
+  constructor(private burgersService : BurgersService) { }
+
+  ngOnInit(): void {
+    this.getBurgers();
+  }
+
+  getBurgers() : void {
+    this.burgerList = this.burgersService.listBurgers()
+        .subscribe(burgerList => this.burgerList = burgerList);
+  }
+}
